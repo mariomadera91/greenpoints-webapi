e7eb6f97a59e16c5704b89ca3b954d3e561a1a17
Victor Montañez <victor.montanez2012@gmail.com>
2021-11-08 23:50:48 -0300

    Cambiar pass por username

--- GreenPoints.Services/DTOs/UsuarioPassUpdateDto.cs
+++ GreenPoints.Services/DTOs/UsuarioPassUpdateDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GreenPoints.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace GreenPoints.Services
 {
     public class UsuarioPassUpdateDto
     {
-        //public string UserName { get; set; }
+        public string UserName { get; set; }
         public int UsuarioId { get; set; }
         public string Password { get; set; }
         public string PasswordConfirm { get; set; }
